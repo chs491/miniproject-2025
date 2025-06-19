7c0a126b35292e3d9464ba1545f25dafae18d5c3
chs491 <hyunsoo1702@gmail.com>
2025-06-19 12:20:31 +0900

    미니프로젝트 4일차 커밋

---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMrpSimulatorApp.ViewModels : ObservableObject
+{
+    internal class MainViewModel
+    {
+        private string _greeting;
+
+    public MainViewModel()
+    {
+        Greeting = "MRP 공정관리!";
+    }
+
+    public string Greeting
+    {
+        get => _greeting;
+        set => SettingsProperty(ref _greeting, value);
+    }
+}
+
