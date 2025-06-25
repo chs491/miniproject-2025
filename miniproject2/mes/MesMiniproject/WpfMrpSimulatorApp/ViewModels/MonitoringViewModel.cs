714b35f9c1f9facc817f43437e171508a7fd3196
chs491 <hyunsoo1702@gmail.com>
2025-06-25 17:27:24 +0900

    커밋

---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/ViewModels/MonitoringViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMrpSimulatorApp.ViewModels
+{
+    public partial class MonitoringViewModel : ObservableObject
+    {
+        public MonitoringViewModel(IDialogCoordinator coordinator)
+        {
+            this.dialogCoordinator = coordinator;  // 파라미터값으로 초기화
+        }
+
+    }
+}
