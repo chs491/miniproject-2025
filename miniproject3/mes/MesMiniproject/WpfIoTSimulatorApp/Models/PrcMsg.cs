764649f0e447514b1a4e0294252e8e7ed90c6aea
chs491 <hyunsoo1702@gmail.com>
2025-08-19 14:45:15 +0900

    08.19 커밋

---
+++ miniproject3/mes/MesMiniproject/WpfIoTSimulatorApp/Models/PrcMsg.cs
@@ -0,0 +1,12 @@
+﻿namespace WpfIoTSimulatorApp.Models
+{
+    // json 전송용 객체. 딴데 안써요
+    public class PrcMsg
+    {
+        public string ClientId { get; set; }
+        public string PlantCode { get; set; }
+        public string FacilityId { get; set; }
+        public string TimeStamp { get; set; }
+        public string Flag { get; set; }
+    }
+}
---
+++ miniproject3/mes/MesMiniproject/WpfMrpSimulatorApp/Models/PrcMsg.cs
@@ -0,0 +1,19 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMrpSimulatorApp.Models
+{
+    // json 전송용 객체. 딴데 안써요
+    public class PrcMsg
+    {
+        public string ClientId { get; set; }
+        public string PlantCode { get; set; }
+        public string FacilityId { get; set; }
+        public string TimeStamp { get; set; }
+        public string Flag { get; set; }
+    }
+}
